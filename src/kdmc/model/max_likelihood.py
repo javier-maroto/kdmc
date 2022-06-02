Fix bug and reduce computation when only using ML for testing

src/kdmc/model/max_likelihood.py
@@ -84,7 +84,7 @@ class MaxLikelihoodModel:
                             (np.pi*t*(1-(4*rolloff*t/Ls)*(4*rolloff*t/Ls))/Ls)
             
             h /= np.sum(h)
-            self.h = torch.from_numpy(h)
+            self.h = torch.from_numpy(h).to(self.device, dtype=torch.float32)
         self.h = self.h.view(1,1,-1).repeat(2,1,1)
 
     def filter_signal(self, x):

src/kdmc/train/base.py
@@ -37,50 +37,51 @@ class Trainer(abc.ABC):
         self.root_path = args.root_path
         self.data_path = args.data_path
         
-        self.ml_preds = self.get_ml_preds(self.train_dl, self.test_dl)
+        self.ml_preds = torch.zeros(
+            [len(self.train_dl.dataset) + len(self.test_dl.dataset), get_num_classes(self.dataset)],
+            device=self.device)
+        self.ml_preds = self.get_ml_preds(self.test_dl, self.ml_preds)
         # Gradient clipping
         if args.grad_clip is not None:
             for p in self.net.parameters():
                 p.register_hook(lambda grad: torch.clamp(grad, -args.grad_clip, args.grad_clip))
 
-    def get_ml_preds(self, train_dl, test_dl):
+    def get_ml_preds(self, dl, ml_preds):
         states_path = self.data_path.joinpath('synthetic/signal/states')
         ml_model = MaxLikelihoodModel(states_path, device=self.device)
-        ml_preds = torch.zeros([len(train_dl.dataset) + len(test_dl.dataset), get_num_classes(self.dataset)], device=self.device)
         print('Computing ML predictions')
-        for dl in (train_dl, test_dl):
-            for batch in tqdm(dl):
-                sps = batch['sps'].to(self.device)
-                rolloff = batch['rolloff'].to(self.device)
-                for a in sps.unique():
-                    for b in rolloff.unique():
-                        mask = (sps == a) & (rolloff == b)
-                        if mask.sum() == 0:
-                            continue
-                        x = batch['x'][mask].to(self.device)
-                        snr_ml = batch['snr_filt'][mask].to(self.device)
-                        y = batch['y'][mask].to(self.device)
-                        idx = batch['idx'][mask].to(self.device, dtype=torch.long)
-                        ml_model.create_filter(Ls=a, rolloff=b)
-                        ml_preds_ = ml_model.compute_ml(x, snr_ml)
-                        ml_preds_ = ml_model.adapt_unsupported(ml_preds_, y)
-                        ml_preds[idx] = ml_preds_
+        for batch in tqdm(dl):
+            sps = batch['sps']
+            rolloff = batch['rolloff']
+            for a in sps.unique():
+                for b in rolloff.unique():
+                    mask = ((sps == a) & (rolloff == b)).to(self.device)
+                    if mask.sum() == 0:
+                        continue
+                    x = batch['x'][mask].to(self.device)
+                    snr_ml = batch['snr_filt'][mask].to(self.device)
+                    y = batch['y'][mask].to(self.device)
+                    idx = batch['idx'][mask].to(self.device, dtype=torch.long)
+                    ml_model.create_filter(Ls=a.numpy().astype(np.int), rolloff=b.numpy())
+                    ml_preds_ = ml_model.compute_ml(x, snr_ml)
+                    ml_preds_ = ml_model.adapt_unsupported(ml_preds_, y)
+                    ml_preds[idx] = ml_preds_
         return ml_preds
 
     def get_adv_ml_preds(self, batch, x_adv):
         ml_model = MaxLikelihoodModel(self.data_path.joinpath('synthetic/signal/states'), device=self.device)
-        sps = batch['sps'].to(self.device)
-        rolloff = batch['rolloff'].to(self.device)
+        sps = batch['sps']
+        rolloff = batch['rolloff']
         ml_preds = torch.zeros([x_adv.shape[0], get_num_classes(self.dataset)], device=self.device)
         for a in sps.unique():
             for b in rolloff.unique():
-                mask = (sps == a) & (rolloff == b)
+                mask = ((sps == a) & (rolloff == b)).to(self.device)
                 if mask.sum() == 0:
                     continue
                 x = batch['x'][mask].to(self.device)
                 snr_ml = batch['snr_filt'][mask].to(self.device)
                 y = batch['y'][mask].to(self.device)
-                ml_model.create_filter(Ls=a, rolloff=b)
+                ml_model.create_filter(Ls=a.numpy().astype(np.int), rolloff=b.numpy())
                 ml_preds_ = ml_model.compute_advml(x, x_adv[mask], snr_ml)
                 ml_preds_ = ml_model.adapt_unsupported(ml_preds_, y)
                 ml_preds[mask] = ml_preds_

src/kdmc/train/std.py
@@ -35,36 +35,10 @@ class STDTrainer(Trainer):
 
 class MLSTDTrainer(Trainer):
     """For faster compute the ml predictions are stored. Thus, assumes there is no data augmentation"""
-    def train(self, epoch):
-        print('\nEpoch: %d' % epoch)
-        self.net.train()
-        train_loss = 0
-        correct = 0
-        correct_ml = 0
-        total = 0
-        for batch_idx, batch in enumerate(tqdm(self.train_dl)):
-            inputs, targets = batch['x'].to(self.device), batch['y'].to(self.device)
-            idxs = batch['idx'].to(self.device, dtype=torch.long)
-            ml_preds = self.ml_preds[idxs]
-            outputs = self.net(inputs)
-            self.optimizer.zero_grad()
-            loss = F.cross_entropy(outputs, ml_preds)
-            loss.backward()
-            self.optimizer.step()
-            if self.sch_updt == 'step':
-                self.scheduler.step()
-
-            train_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets.argmax(1)).sum().item()
-            correct_ml += predicted.eq(ml_preds.argmax(1)).sum().item()
-
-        wandb.log({'train.acc': 100.*correct/total, 'train.acc_ml': 100.*correct_ml/total, 'train.loss': train_loss/(batch_idx+1), 'epoch': epoch})
-
-
-class MLSTDTrainer(Trainer):
-    """For faster compute the ml predictions are stored. Thus, assumes there is no data augmentation"""
+    def __init__(self, args, net, trainloader, testloader, optimizer, scheduler, sch_updt, slow_rate=5):
+        super().__init__(args, net, trainloader, testloader, optimizer, scheduler, sch_updt, slow_rate)
+        self.ml_preds = self.get_ml_preds(self.train_dl, self.ml_preds)
+    
     def train(self, epoch):
         print('\nEpoch: %d' % epoch)
         self.net.train()
@@ -100,6 +74,7 @@ class LNRSTDTrainer(Trainer):
     def __init__(self, args, net, trainloader, testloader, optimizer, scheduler, sch_updt, slow_rate=5):
         super().__init__(args, net, trainloader, testloader, optimizer, scheduler, sch_updt, slow_rate)
         n_signals = len(trainloader.dataset) + len(testloader.dataset)
+        self.ml_preds = self.get_ml_preds(self.train_dl, self.ml_preds)
         self.lnr_mask = torch.zeros([n_signals], device=self.device, dtype=torch.bool)
 
     def train(self, epoch):
@@ -111,11 +86,11 @@ class LNRSTDTrainer(Trainer):
         total = 0
         for batch_idx, batch in enumerate(tqdm(self.train_dl)):
             inputs, targets = batch['x'].to(self.device), batch['y'].to(self.device)
-            snrs = batch['snr_filt'].to(self.device)
             idxs = batch['idx'].to(self.device, dtype=torch.long)
             ml_preds = self.ml_preds[idxs]
             if epoch == 1:
-                self.lnr_mask[idxs] = targets.argmax(1) == ml_preds.argmax(1)
+                lnr_mask = targets.argmax(1) == ml_preds.argmax(1)
+                self.lnr_mask[idxs] = lnr_mask
             else:
                 lnr_mask = self.lnr_mask[idxs]
             
@@ -144,6 +119,7 @@ class SelfMLSTDTrainer(Trainer):
     """For faster compute the ml predictions are stored. Thus, assumes there is no data augmentation"""
     def __init__(self, args, net, trainloader, testloader, optimizer, scheduler, sch_updt, slow_rate=5):
         super().__init__(args, net, trainloader, testloader, optimizer, scheduler, sch_updt, slow_rate)
+        self.ml_preds = self.get_ml_preds(self.train_dl, self.ml_preds)
         self.alpha = args.kt_alpha
         if self.alpha is None:
             raise ValueError(f"--kt_alpha not defined")