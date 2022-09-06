ed7efc1eaa991ef8e4a1d7ad2038e73370158d82
Javier Maroto <javiermarotomorales@gmail.com>
2022-09-06 15:56:15 +0200

    Add sh to create experiments

--- src/exec/1.sh
+++ src/exec/1.sh
@@ -1,38 +1,17 @@
 for seed in {0..3}
 do
-python src/main.py --dataset sp0c20 --loss std --n_epochs 100 --sch_gamma 0.95 --id std --batch_size 1024 --seed $seed
-python src/main.py --dataset sp0c20 --loss std_ml --n_epochs 100 --sch_gamma 0.95 --id std_ml --batch_size 1024 --seed $seed
-python src/main.py --dataset sp0c20 --loss std_lnr --n_epochs 100 --sch_gamma 0.95 --id std_lnr --batch_size 1024 --seed $seed
-python src/main.py --dataset sbasic --loss std --n_epochs 100 --sch_gamma 0.95 --id std --batch_size 1024 --seed $seed
-python src/main.py --dataset sbasic --loss std_ml --n_epochs 100 --sch_gamma 0.95 --id std_ml --batch_size 1024 --seed $seed
-python src/main.py --dataset sbasic --loss std_lnr --n_epochs 100 --sch_gamma 0.95 --id std_lnr --batch_size 1024 --seed $seed
-python src/main.py --dataset sawgn2p --loss std --n_epochs 100 --sch_gamma 0.95 --id std --batch_size 1024 --seed $seed
-python src/main.py --dataset sawgn2p --loss std_ml --n_epochs 100 --sch_gamma 0.95 --id std_ml --batch_size 1024 --seed $seed
-python src/main.py --dataset sawgn2p --loss std_lnr --n_epochs 100 --sch_gamma 0.95 --id std_lnr --batch_size 1024 --seed $seed
-
-python src/main.py --dataset sp0c20 --loss at --n_epochs 100 --sch_gamma 0.95 --id at --batch_size 1024 --seed $seed
-python src/main.py --dataset sp0c20 --loss at_ml --n_epochs 100 --sch_gamma 0.95 --id at_ml --batch_size 1024 --seed $seed
-python src/main.py --dataset sbasic --loss at --n_epochs 100 --sch_gamma 0.95 --id at --batch_size 1024 --seed $seed
-python src/main.py --dataset sbasic --loss at_ml --n_epochs 100 --sch_gamma 0.95 --id at_ml --batch_size 1024 --seed $seed
-python src/main.py --dataset sawgn2p --loss at --n_epochs 100 --sch_gamma 0.95 --id at --batch_size 1024 --seed $seed
-python src/main.py --dataset sawgn2p --loss at_ml --n_epochs 100 --sch_gamma 0.95 --id at_ml --batch_size 1024 --seed $seed
-for dataset_size in 100000, 1000000
-do
-python src/main.py --dataset sp0c20 --loss std --n_epochs 100 --sch_gamma 0.95 --id std --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sp0c20 --loss std_ml --n_epochs 100 --sch_gamma 0.95 --id std_ml --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sp0c20 --loss std_lnr --n_epochs 100 --sch_gamma 0.95 --id std_lnr --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sbasic --loss std --n_epochs 100 --sch_gamma 0.95 --id std --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sbasic --loss std_ml --n_epochs 100 --sch_gamma 0.95 --id std_ml --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sbasic --loss std_lnr --n_epochs 100 --sch_gamma 0.95 --id std_lnr --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sawgn2p --loss std --n_epochs 100 --sch_gamma 0.95 --id std --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sawgn2p --loss std_ml --n_epochs 100 --sch_gamma 0.95 --id std_ml --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sawgn2p --loss std_lnr --n_epochs 100 --sch_gamma 0.95 --id std_lnr --batch_size 1024 --dataset_size $dataset_size --seed $seed
-
-python src/main.py --dataset sp0c20 --loss at --n_epochs 100 --sch_gamma 0.95 --id at --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sp0c20 --loss at_ml --n_epochs 100 --sch_gamma 0.95 --id at_ml --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sbasic --loss at --n_epochs 100 --sch_gamma 0.95 --id at --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sbasic --loss at_ml --n_epochs 100 --sch_gamma 0.95 --id at_ml --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sawgn2p --loss at --n_epochs 100 --sch_gamma 0.95 --id at --batch_size 1024 --dataset_size $dataset_size --seed $seed
-python src/main.py --dataset sawgn2p --loss at_ml --n_epochs 100 --sch_gamma 0.95 --id at_ml --batch_size 1024 --dataset_size $dataset_size --seed $seed
-done
+    for loss in std std_ml std_lnr at at_ml at_aml
+    do
+        for dataset in sbasic sawgn2p sp0c20
+        do
+            for spr in 30 25 20
+            do
+                python src/main.py --dataset $dataset --loss $loss --n_epochs 100 --sch_gamma 0.95 --id $loss --batch_size 1024 --seed $seed --atk pgd Linf $spr 0.25 7
+                for dataset_size in 100000 1000000
+                do
+                    python src/main.py --dataset $dataset --loss $loss --n_epochs 100 --sch_gamma 0.95 --id $loss --batch_size 1024 --seed $seed --atk pgd Linf $spr 0.25 7 --dataset_size $dataset_size
+                done
+            done
+        done
+    done
 done
